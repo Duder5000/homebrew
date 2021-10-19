6088f246e01275b54924b613e2e50b331bc03fae
Duder <soames.alex@gmail.com>
2021-10-19 13:30:53 -0700

    more php testing

--- public/test.php
+++ public/test.php
@@ -1,17 +1,17 @@
 <?php
-	// // Initialize Firebase
-	// var config = {
-	// 	apiKey: "FIREBASE_API_KEY",
-	// 	authDomain: "php-crud-4ded8.firebaseapp.com",
-	// 	databaseURL: "https://php-crud-4ded8-default-rtdb.firebaseio.com",
-	// 	projectId: "duder5000homebrew",
-	// 	storageBucket: "php-crud-4ded8.appspot.com",
-	// 	messagingSenderId: "417923713999",
-	// 	appId: "1:417923713999:web:64a8b9713824cfb8e31bdd",
-	// 	measurementId: "G-328BDXCK4D"
-	// };	
+	// Initialize Firebase
+	var config = {
+		apiKey: "FIREBASE_API_KEY",
+		authDomain: "php-crud-4ded8.firebaseapp.com",
+		databaseURL: "https://php-crud-4ded8-default-rtdb.firebaseio.com",
+		projectId: "duder5000homebrew",
+		storageBucket: "php-crud-4ded8.appspot.com",
+		messagingSenderId: "292552701497",
+		appId: "1:292552701497:web:5eb1ef72a42d22373d7fe8",
+		measurementId: ""
+	};	
 
-	// echo "test";
+	echo "test";
 	
-	https://www.tutsmake.com/php-google-firebase-crud-example-tutorial/
+	// https://www.tutsmake.com/php-google-firebase-crud-example-tutorial/
 ?>
