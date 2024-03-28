f9fdf5960a91e0e418bb7f819010ecea7522e653
Duder5000 <Soames.alex@gmail.com>
2024-03-28 11:53:31 -0700

    *

--- public/helldivers/CleanUpNexusXmls.ps1
+++ public/helldivers/CleanUpNexusXmls.ps1
@@ -34,29 +34,30 @@ foreach ($file in $xmlFiles) {
 Write-Host "File renaming process complete."
 
 #########################################################################
+#https://github.com/SublimeText/PowerShell
 
-$nexusPath = "C:\Users\Duder5000\Documents\homebrew\public\downloads\nexus"
-$hd2Path = "C:\Users\Duder5000\Documents\homebrew\public\downloads\hd2"
-$noMatchPath = "C:\Users\Duder5000\Documents\homebrew\public\downloads\noMatch"
-$noMatchListFile = "C:\Users\Duder5000\Documents\homebrew\public\downloads\noMatchList.txt"
-
-if (-not (Test-Path -Path $noMatchPath)) {
-    New-Item -Path $noMatchPath -ItemType Directory | Out-Null
-}
-
-$xmlFiles = Get-ChildItem -Path $nexusPath -Filter "*.xml" -File
-$noMatchFiles = @()
-
-foreach ($xmlFile in $xmlFiles) {
-    $matchingFile = Get-ChildItem -Path $hd2Path -Filter $xmlFile.Name -File
-
-    if (-not $matchingFile) {
-        Move-Item -Path $xmlFile.FullName -Destination $noMatchPath -Force
-        Write-Output "Moved $($xmlFile.Name) to $noMatchPath"
-        $noMatchFiles += $xmlFile.Name
-    }
-}
-
-$noMatchFiles | Out-File -FilePath $noMatchListFile -Force
-
-Write-Host "File moving complete."
+#$nexusPath = "C:\Users\Duder5000\Documents\homebrew\public\downloads\nexus"
+#$hd2Path = "C:\Users\Duder5000\Documents\homebrew\public\downloads\hd2"
+#$noMatchPath = "C:\Users\Duder5000\Documents\homebrew\public\downloads\noMatch"
+#$noMatchListFile = "C:\Users\Duder5000\Documents\homebrew\public\downloads\noMatchList.txt"
+#
+#if (-not (Test-Path -Path $noMatchPath)) {
+#    New-Item -Path $noMatchPath -ItemType Directory | Out-Null
+#}
+#
+#$xmlFiles = Get-ChildItem -Path $nexusPath -Filter "*.xml" -File
+#$noMatchFiles = @()
+#
+#foreach ($xmlFile in $xmlFiles) {
+#    $matchingFile = Get-ChildItem -Path $hd2Path -Filter $xmlFile.Name -File
+#
+#    if (-not $matchingFile) {
+#        Move-Item -Path $xmlFile.FullName -Destination $noMatchPath -Force
+#        Write-Output "Moved $($xmlFile.Name) to $noMatchPath"
+#        $noMatchFiles += $xmlFile.Name
+#    }
+#}
+#
+#$noMatchFiles | Out-File -FilePath $noMatchListFile -Force
+#
+#Write-Host "File moving complete."
