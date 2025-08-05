223e9673716f4a8982997ddd241e4f4b0fa87a6b
Duder5000 <Soames.alex@gmail.com>
2025-08-05 11:57:36 -0700

    Duder5000 Tweaks 1.0.5
    Added toggle to settings

--- public/duder5000-tweaks/scripts/heal-on-rest.js
+++ public/duder5000-tweaks/scripts/heal-on-rest.js
@@ -1,6 +1,22 @@
+Hooks.once("init", () => {
+  game.settings.register("duder5000-tweaks", "enableCustomRest", {
+    name: "Enable Full Heal on Rest",
+    hint: "Enabling this will change how much HP is recovered when a character rests, restoring them to full HP.",
+    scope: "world",
+    config: true,
+    default: true,
+    type: Boolean
+  });
+});
+
+
 Hooks.on("pf2e.restForTheNight", async (actor) => {
   const current = actor.system.attributes.hp.value;
   const max = actor.system.attributes.hp.max;
+  const delta = max - current;
+
+  const isEnabled = game.settings.get("duder5000-tweaks", "enableCustomRest");
+  if (!isEnabled) return;
 
   if (current < max) {
     await actor.update({ "system.attributes.hp.value": max });
