deb367a04cf74c8e578cf77b728791d5ee0cdaeb
Duder5000 <Soames.alex@gmail.com>
2025-08-12 11:18:21 -0700

    Tweaks 1.0.76
    Updated chat message

--- public/duder5000-tweaks/scripts/heal-on-rest.js
+++ public/duder5000-tweaks/scripts/heal-on-rest.js
@@ -11,9 +11,17 @@ Hooks.once("init", () => {
 
 
 Hooks.on("pf2e.restForTheNight", async (actor) => {
+  const conModifier = actor.system.abilities.con.mod;
+  const level = actor.system.details.level.value;
+  const recoveryMultiplier = actor.system.attributes.hp.recoveryMultiplier;
+  const recoveryAddend = actor.system.attributes.hp.recoveryAddend;
+
   const current = actor.system.attributes.hp.value;
   const max = actor.system.attributes.hp.max;
-  const delta = max - current;
+  
+  const normMaxRestored = Math.max(conModifier, 1) * level * recoveryMultiplier + recoveryAddend;
+
+  const delta = max - current + normMaxRestored;
 
   const isEnabled = game.settings.get("duder5000-tweaks", "enableCustomRest");
   if (!isEnabled) return;
@@ -37,7 +45,8 @@ Hooks.on("pf2e.restForTheNight", async (actor) => {
 
     ChatMessage.create({
       speaker: ChatMessage.getSpeaker({ actor }),
-      content: `<p><strong>${actor.name}</strong> fully recovers to <strong>${max} HP</strong> after resting for the night.</p>`
+      // content: `<p><strong>${actor.name}</strong> fully recovers to <strong>${max} HP</strong> after resting for the night.</p>`
+      content: `<p><strong>${actor.name}</strong> rested for the night, recovering <strong>${delta} HP</strong>.</p>`
     });
   }
 });
